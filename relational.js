31b4ebe92fbed2ca09bb2c48961bfdc84baabaef
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-09-01 12:15:22 +0600

    Relational/Comaprison Operatro

---
+++ relational.js
@@ -0,0 +1,17 @@
+let x = 10,
+  y = 15,
+  o = 10;
+
+console.log(x > y); // false
+console.log(x < y); // true
+console.log(x > o); // false
+console.log(x >= o); //true
+console.log(x >= y); // false
+console.log(x <= y); // true
+console.log(x <= o); // true
+console.log(x == y); // false
+console.log(x === y); //false
+console.log(x !== y); // true
+console.log(x == o); // true
+console.log(x === o); // true;
+console.log(x !== o); // false
