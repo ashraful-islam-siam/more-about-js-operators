f45c753cf6ac715a4dac3a4ad7ed927489446967
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-09-01 11:00:13 +0600

    Arithmetic done

--- arithmetic.js
+++ arithmetic.js
@@ -7,3 +7,15 @@ console.log(b - a);
 console.log(a * b * c);
 console.log(a / b);
 console.log(a % b);
+
+let r1 = a + b + c;
+let r2 = c * b * a;
+let r3 = a % 3;
+let r4 = (a + b + c) / 3;
+let r5 = a + b - c;
+
+console.log(r1);
+console.log(r2);
+console.log(r3);
+console.log(r4);
+console.log(r5);
